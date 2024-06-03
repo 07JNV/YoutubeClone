fd67eeedbaf379fc08861b1710c9837b9958389d
07jnv <s1achin8958@gmail.com>
2024-06-03 11:51:31 +0530

    donesomechanges

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,13 @@
-import express from 'express'
+import express from "express";
 
-import {login} from '../controllers/auth.js'
-import {updateChanelData,getAllChanels} from '../controllers/chanel.js'
+import { login } from "../controllers/auth.js";
 
-
+import { updateChanelData, getAllChanels } from "../controllers/chanel.js";
 
 const routes = express.Router();
 
-routes.post('/login',login)
-routes.patch('/update/:id',updateChanelData)
-routes.get('/getAllChanels',getAllChanels)
+routes.post("/login", login);
+routes.patch("/update/:id", updateChanelData);
+routes.get("/getAllChanels", getAllChanels);
 
 export default routes;
