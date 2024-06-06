ab4d2f3b7a9e430f0b4c9efe0c08a94a9fcf0df9
07jnv <s1achin8958@gmail.com>
2024-06-06 12:09:45 +0530

    changeorigintotrue

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-    origin: 'https://66615757239cf752cfd94b8f--hilarious-daffodil-379d3e.netlify.app', // allow requests from this origin
+    origin: true, // allow requests from any origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204
