9265c94d9e75796cf054a83f2c120dbb085bbb86
07jnv <s1achin8958@gmail.com>
2024-06-14 21:15:25 +0530

    cors

--- index.js
+++ index.js
@@ -1,3 +1,75 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import cors from 'cors';
+// import bodyParser from "body-parser";
+// import userRoutes from './routes/user.js';
+// import videoRoutes from './routes/video.js';
+// import commentsRoutes from './routes/comments.js';
+// import path from 'path';
+// // import User from "./models/auth.js"
+
+// dotenv.config();
+
+// const app = express();
+
+// // CORS configuration
+// // const corsOptions = {
+// //     origin: true, // allow requests from any origin
+// //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// //     credentials: true,
+// //     optionsSuccessStatus: 204
+// // };
+
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         const allowedOrigin = 'https://youtubefrontjnv.netlify.app';
+//         if (origin === allowedOrigin || !origin) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//     optionsSuccessStatus: 204
+// };
+
+// app.use(cors(corsOptions));
+
+// app.use(express.json({ limit: "30mb", extended: true }));
+// app.use(express.urlencoded({ limit: "30mb", extended: true }));
+// app.use('/uploads', express.static(path.join('uploads')));
+
+// app.get('/', (req, res) => {
+//     res.send("hello");
+// });
+
+
+
+
+// app.use(bodyParser.json());
+
+// app.use('/user', userRoutes);
+// app.use('/video', videoRoutes);
+// app.use('/comment', commentsRoutes);
+
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//     console.log(`Server Running on the PORT ${PORT}`);
+// });
+
+// const DB_URL = process.env.CONNECTION_URL;
+// mongoose.set("strictQuery", false);
+// mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+//     console.log("MongoDB database connected");
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+
+
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -14,17 +86,13 @@ dotenv.config();
 const app = express();
 
 // CORS configuration
-// const corsOptions = {
-//     origin: true, // allow requests from any origin
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-//     optionsSuccessStatus: 204
-// };
-
 const corsOptions = {
     origin: function (origin, callback) {
-        const allowedOrigin = 'https://youtubefrontjnv.netlify.app';
-        if (origin === allowedOrigin || !origin) {
+        const allowedOrigins = [
+            'https://youtubefrontjnv.netlify.app',
+            'https://youtubeclonefront--youtubefrontjnv.netlify.app'
+        ];
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
@@ -45,9 +113,6 @@ app.get('/', (req, res) => {
     res.send("hello");
 });
 
-
-
-
 app.use(bodyParser.json());
 
 app.use('/user', userRoutes);
@@ -66,5 +131,3 @@ mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).th
 }).catch((error) => {
     console.log(error);
 });
-
-
